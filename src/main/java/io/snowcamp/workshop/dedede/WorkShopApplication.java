e93fa2316116afd680c043bc0e1d0d3a5d861841
Maxime Bouverat <maximebouverat@gmail.com>
2024-09-13 17:15:57 +0200

    Empty application

---
+++ src/main/java/io/snowcamp/workshop/dedede/WorkShopApplication.java
@@ -0,0 +1,13 @@
+package io.snowcamp.workshop.dedede;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/snowcamp/workshop/dedede/WorkShopApplicationTests.java
@@ -0,0 +1,13 @@
+package io.snowcamp.workshop.dedede;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
